b5faeedcbdc61d9ed804e177c99a5d3430fadc55
Jason Cypret <jason@jasoncypret.com>
2017-09-21 10:44:51 -0400

    Adding a new path to test on rails

--- lib/nitro_sg/engine.rb
+++ lib/nitro_sg/engine.rb
@@ -9,5 +9,7 @@ module NitroSg
     config.sass.load_paths ||= []
     config.sass.load_paths << "#{Gem.loaded_specs['bootstrap-sass'].full_gem_path}/assets/stylesheets"
     config.sass.load_paths << "#{Gem.loaded_specs['bootstrap-sass'].full_gem_path}/assets/stylesheets/bootstrap"
+    config.sass.load_paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/sass-mixins"
+    config.sass.load_paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/components"
   end
 end
