bd73f85d00e22004faa3dacc6b7924e2e75defdf
Stephen Marshall <stephen.marshall@powerhrg.com>
2017-09-21 11:12:47 -0400

    Fix thangs

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 
 import * as basicComponents from "./basic";
 
+import "../app/assets/stylesheets/nitro_sg/application.scss";
+
 const addStories = function(stories) {
   let newStories = storiesOf(stories.title, module);
   keys(stories.components).forEach(function (key) {
