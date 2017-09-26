fcfacca0dd0d870101aa170f29a13543ef2b687d
Markie Arnold <markiearnold@gmail.com>
2017-09-26 12:31:17 -0500

    Update powuhbuilda.webpack.config.js

--- .storybook/powuhbuilda.webpack.config.js
+++ .storybook/powuhbuilda.webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
            loader: 'sass-loader',
            options: {
              includePaths: [
-               path.resolve(__dirname, '../node_modules/nitro-theme/app/assets/stylesheets/nitro_theme')
+               path.resolve(__dirname, '../sass-mixins')
              ]
            }
          }
