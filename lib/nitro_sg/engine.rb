2ca5496eaa794b6929996414a301fd6e52a6c5b7
Jason Cypret <jason@jasoncypret.com>
2018-05-17 12:40:51 -0500

    engine needs the JS...testing this

--- lib/nitro_sg/engine.rb
+++ lib/nitro_sg/engine.rb
@@ -7,7 +7,8 @@ module NitroSg
     end
 
     config.sass.load_paths ||= []
-    config.sass.load_paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/fonts"
+    config.assets.paths ||= []
+    config.assets.paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/fonts"
     config.sass.load_paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/sass-mixins"
     config.sass.load_paths << "#{Gem.loaded_specs['nitro_sg'].full_gem_path}/components"
   end
