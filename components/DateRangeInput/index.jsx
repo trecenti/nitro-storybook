e30b27e36c5255671962f9ea18ae3f5d8212dbe0
Stephen Marshall <stephen.marshall@powerhrg.com>
2018-06-12 16:23:51 -0500

    Handle only fromDate or toDate

--- components/DateRangeInput/index.jsx
+++ components/DateRangeInput/index.jsx
@@ -14,7 +14,18 @@ export type Props = {
 const DateRangeInput = ({ className, fromDate, toDate, isValidDate, viewDate, ...props }) => {
   className = classnames(className, styles.dob)
 
-  const isWithinRange = currentDate => currentDate.isAfter(fromDate) && currentDate.isBefore(toDate)
+  const isWithinRange = currentDate => {
+    const afterFromDate = currentDate.isAfter(fromDate)
+    const beforeToDate = currentDate.isBefore(toDate)
+
+    if (fromDate && toDate) {
+      return afterFromDate && beforeToDate
+    } else if (fromDate) {
+      return afterFromDate
+    } else {
+      return beforeToDate
+    }
+  }
 
   props.viewDate = viewDate || moment()
   props.isValidDate = isValidDate || isWithinRange
